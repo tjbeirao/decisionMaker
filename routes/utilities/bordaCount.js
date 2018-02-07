337f558a8b5e05f8a752c825dec7ffa1e6eb2d69
tjbeirao <tjbeirao@gmail.com>
2018-02-07 21:18:06 +0000

    structure of server.js commented

---
+++ routes/utilities/bordaCount.js
@@ -0,0 +1,17 @@
+module.exports = function (answers) {
+
+    function bordaMath(surveyid, answers) {
+        // Get anwser id from DB
+        // let surveyID = 
+
+        // Verify anwser position on drag-and-drop
+        // jquery
+
+        // Make the math
+        // get 1st position of drag-and drop
+        // for (let i = 0; i < answers.length - 1; i++ )
+            //_edit.database(survey id x. answers id y .SUM(answers[i]))
+        // Call the function to save the SUM of the new number to the database
+
+    }
+}
--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
 const randomUrl = require("./routes/utilities/randomUrl.js");
-const cookieSession = require('cookie-session')
+const cookieSession = require('cookie-session');
+const borda = require("./routes/utilities/bordaCount.js");
+const dbHelpers = require("./db/dbHelpers.js")(knex)
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
@@ -47,15 +49,14 @@ app.use(cookieSession({
 // Mount all resource routes
 // app.use("/api/users", usersRoutes(knex));
 
-
-
 app.get("/", (req, res) => {
   res.render("./index")
 })
 
 app.post("/", (req, res) => {
-  //collect email from HTML input, save it to the data base on [admin:email]
+  //collect email from HTML input, save it to the data base on [addAdmin]
   //create a cookie with email information
+  //let current_user = req.session.current_user
   res.redirect("/index/create")
 })
 
@@ -64,9 +65,12 @@ app.get("/create", (req, res) => {
 })
 
 app.post("/create", (req, res) => {
-  //collect information from HTML text inputs, save it to the data base [poll: title, subject, question, anwsers]
+  //collect information from HTML text inputs, save it to the data base [addSurveyInfo]
   //call function to generate random URL - twice 
-  //randomUrl()
+  //let adminlink = randomUrl()
+  // let userlink = randomUrl()
+  // dbHelpers.addlinks(adminlink, userlink)
+  //save random URL on database [addLinks]
   //send email for using the saved cookie
   res.redirect("/create/confirmation")
 })
@@ -83,17 +87,21 @@ app.get("/survey/:user_survey_id", (req, res) => {
 
 app.post("/survey/:user_survey_id", (req, res) => {
   //collect informations from drag-and-drop
+  //[searchResultsScore]
   //run the function to do the math of the results
   //save the results in database
+  //[addResultsScore]
+  //search for admin email 
+  //collect information from DB [searchForAdminLink]
+  //send the email for the admin
   res.redirect("/survey/confirmation")
 })
 
-app.get("/survey/:admin_survey_id"){
-  //collect information from DB
-  //convert the data for percentage
+app.get("/survey/:admin_survey_id", (req, res) =>{
+  //pull data from database [addResultsInfo]
+  //convert the data to percentage 
   //sort the collection for display
-}
-
+})
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
