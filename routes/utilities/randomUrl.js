e5c50bd3bd23b0b8d4056b3952325b031812736f
tjbeirao <tjbeirao@gmail.com>
2018-02-08 23:12:57 +0000

    function to open URL with user link working

--- routes/utilities/randomUrl.js
+++ routes/utilities/randomUrl.js
@@ -5,13 +5,11 @@ module.exports = function (type) {
         return randomize('aA0', 16)
     }
     
-    if (type == user){
-        const newUrl = getUrl();
-        newUrl = (`http://localhost:8080/decidekick/survey/${type}`)
+    if (type == 'user'){
+        let newUrl = (`http://localhost:8080/survey/${getUrl()}`)
         return newUrl;
-    } else if (type == admin) {
-        const newUrl = getUrl();
-        newUrl = (`http://localhost:8080/decidekick/admin/${type}`)
+    } else if (type == 'admin') {
+        let newUrl = (`http://localhost:8080/admin/${getUrl()}`)
         return newUrl;
     }
 }
